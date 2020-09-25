d2471cfeefc649811108cbbafab36e2a3cd20aaa
Matthew B. Jones <jones@nceas.ucsb.edu>
2020-09-25 19:00:02 +0000

    version 1.0.17-12

--- inst/doc/redland_overview.R
+++ inst/doc/redland_overview.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(redland)
 world <- new("World")
 storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
@@ -7,7 +7,7 @@ parser <- new("Parser", world)
 parseFileIntoModel(parser, world, system.file("extdata", "dc.rdf", package="redland"), model)
   
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 queryString <- 'PREFIX dc: <http://purl.org/dc/elements/1.1/> SELECT ?a ?c WHERE { ?a dc:creator ?c . }'
 query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
 queryResult <- executeQuery(query, model)
@@ -15,14 +15,14 @@ results <- getResults(query, model, "rdfxml")
 
 cat(sprintf("Results from query: %s\n", results))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 stmt <- new("Statement", world=world, 
         subject="http://www.dajobe.org/",
         predicate="http://purl.org/dc/elements/1.1/language",
         object="en")
 addStatement(model, stmt)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 serializer <- new("Serializer", world, mimeType="application/rdf+xml")
 status <- setNameSpace(serializer, world, namespace="http://purl.org/dc/elements/1.1/", prefix="dc")  
 filePath <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".rdf")
